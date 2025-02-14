375b580e1c7d199052e4fa9602063ce60945b319
deslrey <2135232170@qq.com>
2025-02-14 17:30:57 +0800

    adding global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Navbar from './components/NavBar';
+import { Metadata } from 'next';
+import '../app/styles/globals.css'
 
-export const metadata = {
+export const metadata: Metadata = {
     title: 'deslre',
     description: '个人博客',
 };
@@ -10,6 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <html lang="en">
             <head />
             <body>
+                <Navbar />
                 {children}
             </body>
         </html>
