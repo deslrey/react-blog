d26db9e6109fa7fd34e4734607510e1e4f23eda0
deslrey <2135232170@qq.com>
2025-02-14 17:16:45 +0800

    modify the icon file

--- src/app/components/SysIcon/index.tsx
+++ src/app/components/SysIcon/index.tsx
@@ -1,7 +1,8 @@
+"use client";
 import { createFromIconfontCN } from "@ant-design/icons";
 
 const SysIcon = createFromIconfontCN({
-    scriptUrl: ['//at.alicdn.com/t/c/font_4827535_znorwqhc0x.js'],
+    scriptUrl: ['//at.alicdn.com/t/c/font_4827535_plhmtwigrol.js'],
 })
 
 export default SysIcon;
