897c673d98e892757892a1d6ce3302419e7c4a33
deslrey <2135232170@qq.com>
2025-02-15 22:01:52 +0800

    feat: implement navigation bar click jump

--- src/app/components/NavBar/index.tsx
+++ src/app/components/NavBar/index.tsx
@@ -1,16 +1,23 @@
+'use client'
 import React from 'react'
 import navList, { NvaObj } from './router';
 import SysIcon from '../SysIcon';
 import styles from "./navBar.module.css";
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
 
+    const pathName = usePathname()
+
     const nvaItem = (obj: NvaObj) => {
         return (
-            <div key={obj.key}>
-                <SysIcon type={obj.icon} />
-                <span>{obj.title}</span>
-            </div>
+            <Link className={`${styles.nav_item} nav_item_text ${pathName !== '/' && pathName === obj?.href ? styles.nav_item_active : ''}`}
+                key={obj?.key}
+                href={obj?.href}>
+                <SysIcon className={styles.nav_item_icon} type={obj?.icon} />
+                <span className={styles.nav_item_title}>{obj?.title}</span>
+            </Link>
         )
     }
 
