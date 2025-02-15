c4cea192fe48a3719fb3962834b5c3c0618b7b40
deslrey <2135232170@qq.com>
2025-02-15 10:47:09 +0800

    feat: typewriter Effect

--- src/app/Home/Home.tsx
+++ src/app/Home/Home.tsx
@@ -1,11 +1,36 @@
 'use client'
 
 import React from 'react'
+import { useEffect, useRef } from "react"
 import BgImage from '../../../public/images/bg05.jpeg'
 import styles from './home.module.css'
+import Typed from "typed.js"
 import Image from 'next/image'
+import SysIcon from '../components/SysIcon'
 
 const Home = () => {
+
+    const typeTarget = useRef<any>(null);
+
+    useEffect(() => {
+        const typed = new Typed(typeTarget.current, {
+            strings: [
+                "个人Blog",
+                "别把最好的时光浪费在无谓的等待和犹豫不决中",
+            ],
+            typeSpeed: 60,
+            backSpeed: 40,
+            loop: true,
+            loopCount: Infinity,
+            autoInsertCss: false,
+            backDelay: 2000,
+            showCursor: false,
+        });
+
+        return () => {
+            typed.destroy();
+        };
+    }, []);
 
     return (
         <div className={styles.home}>
@@ -17,6 +42,20 @@ const Home = () => {
                     src={BgImage}
                     alt='blog-bg'
                     priority={true} />
+            </div>
+            <div className={styles.bg_mask} id="bg_mask" />
+            <div className={styles.bg_content}>
+                <div className={styles.title}>世人万千，再难遇我</div>
+                <div className={styles.description_box}>
+                    <div className={styles.description} ref={typeTarget} />
+                </div>
+                <div className={styles.jiantou}>
+                    <SysIcon
+                        className={styles.jiantou_icon}
+                        type="icon-arrow-down"
+                    // onClick={goAbout}
+                    />
+                </div>
             </div>
         </div>
     )
--- src/app/components/SysIcon/index.tsx
+++ src/app/components/SysIcon/index.tsx
@@ -2,7 +2,7 @@
 import { createFromIconfontCN } from "@ant-design/icons";
 
 const SysIcon = createFromIconfontCN({
-    scriptUrl: ['//at.alicdn.com/t/c/font_4827535_plhmtwigrol.js'],
+    scriptUrl: ['//at.alicdn.com/t/c/font_4827535_v8q1zauw0l.js'],
 })
 
 export default SysIcon;
