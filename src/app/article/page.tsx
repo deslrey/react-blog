2b58a5d738fc398baa7b48f0e318b784582014d1
deslrey <2135232170@qq.com>
2025-03-21 21:40:35 +0800

    feat: add test data

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,56 +1,62 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link';
 import Image, { StaticImageData } from 'next/image'
 import ArticleImage from '../../../public/images/0.webp'
 
 import styles from './article.module.css'
 
-
 interface ArticleObj {
-    id: number,
     title: string,
-    description: string,
-    image: StaticImageData,
+    author: string,
+    describe: string,
+    update: string,
+    readTime: string,
+    type: string,
+    fileName: number,
 }
-
-const ArticleList: ArticleObj[] = [
-    { id: 1, title: 'Article 1', description: 'Description 1', image: ArticleImage },
-    { id: 2, title: 'Article 2', description: 'Description 2', image: ArticleImage },
-    { id: 3, title: 'Article 3', description: 'Description 3', image: ArticleImage },
-]
-
 
 const ArticleItem = ({ article }: { article: ArticleObj }) => {
     return (
-        <Link href={`/blog-details/${article.id}`} className={styles.link}>
+        <Link href={`/blog-details/${article.fileName}`} className={styles.link}>
             <div className={styles.articleItem}>
                 <Image
-                    src={article.image}
+                    src={ArticleImage}
                     alt={article.title}
                     className={styles.articleImage}
                 />
 
                 <div className={styles.articleContent}>
                     <h1>{article.title}</h1>
-                    <p>{article.description}</p>
+                    <p>{article.describe}</p>
                 </div>
             </div>
         </Link>
     )
 }
 
-
-
 function Article() {
+    const [articles, setArticles] = useState<ArticleObj[]>([]);
+
+    // 使用 useEffect 加载 JSON 数据
+    useEffect(() => {
+        const loadArticles = async () => {
+            const response = await fetch('/api/list.json');
+            const data = await response.json();
+            setArticles(data); // 设置为文章数组
+        };
+
+        loadArticles();
+    }, []);
+
     return (
-        <>
-            <div className={styles.article}>
-                {ArticleList.map((article) => (
-                    <ArticleItem key={article.id} article={article} />
-                ))}
-            </div>
-        </>
-    )
+        <div className={styles.article}>
+            {articles.map((article) => (
+                <ArticleItem key={article.fileName} article={article} />
+            ))}
+        </div>
+    );
 }
 
-export default Article
+export default Article;
