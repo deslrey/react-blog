d58876584c21d7f9eb2fdb78d721f25c9d15c2c9
deslrey <2135232170@qq.com>
2025-03-22 16:31:35 +0800

    feat: about basic style

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,16 @@
-import React from 'react'
-
-import styles from "./about.module.css";
+import React from 'react';
+import styles from './about.module.css';
 
 const About = () => {
     return (
-        <div className={styles.about}>我是关于界面</div>
-    )
+        <div className={styles.about}>
+            <div className={styles.avatar}></div>
+            <div className={styles.aboutContent}>
+                <h1>关于我</h1>
+                <p>这里是关于我的一些信息。</p>
+            </div>
+        </div>
+    );
 }
 
 export default About;
-
