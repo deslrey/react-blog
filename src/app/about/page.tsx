5c5318766bddacf86a4fb1e38dc72514f5b03323
deslrey <2135232170@qq.com>
2025-03-22 16:53:54 +0800

    feat: add github link

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import styles from './about.module.css';
+import SysIcon from '../components/SysIcon';
 
 const About = () => {
     return (
         <div className={styles.about}>
             <div className={styles.avatar}></div>
+            <a href="https://github.com/deslrey" target="_blank" rel="noopener noreferrer">
+                <SysIcon
+                    style={{ fontSize: '40px', color: '#000' }}
+                    type="icon-github1"
+                />
+            </a>
             <div className={styles.aboutContent}>
-                <h1>关于我</h1>
                 <p>这里是关于我的一些信息。</p>
             </div>
         </div>
