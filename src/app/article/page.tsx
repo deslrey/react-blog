4ce83fae06b7e4f8d90895d81b182371e40712b1
deslrey <2135232170@qq.com>
2025-04-16 11:51:35 +0800

    feat: getArticleList data

--- src/app/article/page.tsx
+++ src/app/article/page.tsx
@@ -1,25 +1,18 @@
 'use client';
 
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Link from 'next/link';
 import Image, { StaticImageData } from 'next/image'
 import ArticleImage from '../../../public/images/0.webp'
 
 import styles from './article.module.css'
+import request from '../utils/Request';
+import { Article as ArticleObj } from '../interfaces/bolgDetails';
 
-interface ArticleObj {
-    title: string,
-    author: string,
-    describe: string,
-    update: string,
-    readTime: string,
-    type: string,
-    fileName: number,
-}
 
 const ArticleItem = ({ article }: { article: ArticleObj }) => {
     return (
-        <Link href={`/blog-details/${article.fileName}`} className={styles.link}>
+        <Link href={`/blog-details/${article.id}`} className={styles.link}>
             <div className={styles.articleItem}>
                 <Image
                     src={ArticleImage}
@@ -29,7 +22,7 @@ const ArticleItem = ({ article }: { article: ArticleObj }) => {
 
                 <div className={styles.articleContent}>
                     <h1>{article.title}</h1>
-                    <p>{article.describe}</p>
+                    <p>{article.description}</p>
                 </div>
             </div>
         </Link>
@@ -39,11 +32,28 @@ const ArticleItem = ({ article }: { article: ArticleObj }) => {
 function Article() {
     const [articles, setArticles] = useState<ArticleObj[]>([]);
 
+    const sign = useRef<boolean>(false); // 用于标记是否已经加载过数据
+
     // 使用 useEffect 加载 JSON 数据
     useEffect(() => {
+
+        if (sign.current) return; // 如果已经加载过数据，则不再加载
+        sign.current = true; // 设置标记为 true，表示数据已加载
+
         const loadArticles = async () => {
-            const response = await fetch('/api/list.json');
-            const data = await response.json();
+
+            const result = await request.get<ArticleObj[]>("/article/getArticleList")
+
+            if (result.code !== 200) {
+                console.error("获取文章列表失败", result.message);
+                return;
+
+            }
+
+            // console.log('result ======> ', result);
+
+            const data = result.data;
+
             setArticles(data); // 设置为文章数组
         };
 
@@ -53,7 +63,7 @@ function Article() {
     return (
         <div className={styles.article}>
             {articles.map((article) => (
-                <ArticleItem key={article.fileName} article={article} />
+                <ArticleItem key={article.id} article={article} />
             ))}
         </div>
     );
