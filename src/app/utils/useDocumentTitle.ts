53566c4efe5203aa06299dba1ca7416e34dc401e
deslrey <2135232170@qq.com>
2025-04-30 23:11:46 +0800

    feat: modify title tool

---
+++ src/app/utils/useDocumentTitle.ts
@@ -0,0 +1,22 @@
+'use client'
+import { useEffect } from 'react'
+
+export default function useDocumentTitle(title: string, description?: string) {
+    useEffect(() => {
+        if (title) {
+            document.title = title
+        }
+
+        if (description) {
+            const metaDesc = document.querySelector("meta[name='description']")
+            if (metaDesc) {
+                metaDesc.setAttribute('content', description)
+            } else {
+                const meta = document.createElement('meta')
+                meta.name = 'description'
+                meta.content = description
+                document.head.appendChild(meta)
+            }
+        }
+    }, [title, description])
+}
